ade0a91e61d6845c8b5c3ea4ca765fd57d0dc02c
DmShko <vombat_schev@i.ua>
2023-06-11 16:24:23 +0300

    vomgall-14

--- js/modal.js
+++ js/modal.js
@@ -4,13 +4,33 @@
       openModalComtactsLink: document.querySelector("[data-modal-contacts-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+      modalContentAbout: document.querySelector("[about-content-is-hidden]"),
+      modalContentContacts: document.querySelector("[contacts-content-is-hidden]"),
     };
   
-    refs.openModalAboutLink.addEventListener("click", toggleModal);
-    refs.openModalComtactsLink.addEventListener("click", toggleModal);
+    refs.openModalAboutLink.addEventListener("click", toggleModalAbout);
+    refs.openModalComtactsLink.addEventListener("click", toggleModalContacts);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      if(!refs.modalContentAbout.classList.contains("is-about")) {
+        refs.modalContentAbout.classList.toggle("is-about");
+      }
+      if(!refs.modalContentContacts.classList.contains("is-contacts")) {
+        refs.modalContentContacts.classList.toggle("is-contacts");
+      }
+    }
+
+    function toggleModalAbout() {
+      refs.modal.classList.toggle("is-hidden");
+      // refs.modalContentContacts.classList.toggle("is-contacts");
+      refs.modalContentAbout.classList.toggle("is-about");
+    }
+
+    function toggleModalContacts() {
+      refs.modal.classList.toggle("is-hidden");
+      // refs.modalContentAbout.classList.toggle("is-about");
+      refs.modalContentContacts.classList.toggle("is-contacts");
     }
   })();
