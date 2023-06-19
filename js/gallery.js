d033b15c7e9d1d3181f9061b07c363b139d8b034
DmShko <vombat_schev@i.ua>
2023-06-19 22:13:06 +0300

    vomgall-16

--- js/gallery.js
+++ js/gallery.js
@@ -7,6 +7,14 @@
         header: document.querySelector("[change-color]"),
         
         nav: document.querySelector("[change-border]"),
+
+        // Open image
+        imageLimkClick: document.querySelector("[oil-click-images]"),
+        imageOpen: document.querySelector("[oil-open-images]"),
+
+        imageClose: document.querySelector("[oil-images-modal-close]"),
+
+        nav: document.querySelector("[change-border]"),
     }
 
     refs.headerLimkHoverFocus.addEventListener("mouseover", toggleModal);
@@ -15,8 +23,22 @@
     refs.addressLimkHoverFocus.addEventListener("mouseover", toggleModal);
     refs.addressLimkHoverFocus.addEventListener("mouseout", toggleModal);
 
+    // Open image link event
+    refs.imageLimkClick.addEventListener("click", openGallModal);
+
+    // Close image link event
+    refs.imageClose.addEventListener("click", closeGallModal);
+
     function toggleModal() {
         refs.header.classList.toggle("is-hover");
         refs.nav.classList.toggle("is-border");
       }
+
+    function openGallModal() {
+        refs.imageOpen.classList.toggle("is-oil-open");
+    }
+
+    function closeGallModal() {
+        refs.imageOpen.classList.toggle("is-oil-open");
+    }
 })();
