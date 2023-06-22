96cc55129df215efa8e2ea2fb94e40f3c5abf7ee
DmShko <vombat_schev@i.ua>
2023-06-22 22:50:13 +0300

    vomgall-17

--- js/gallery.js
+++ js/gallery.js
@@ -14,6 +14,10 @@
 
         imageClose: document.querySelector("[oil-images-modal-close]"),
 
+        imagesContainerList: document.getElementsByClassName('gallery-oil-link'),
+
+        backdropImagesContainer: document.querySelector('.oil-modal-container'),
+
         nav: document.querySelector("[change-border]"),
     }
 
@@ -24,7 +28,11 @@
     refs.addressLimkHoverFocus.addEventListener("mouseout", toggleModal);
 
     // Open image link event
-    refs.imageLimkClick.addEventListener("click", openGallModal);
+    for(const element of refs.imagesContainerList) {
+        // add event for each images link of list
+        element.addEventListener("click", openGallModal);
+        // console.log(element);
+    }
 
     // Close image link event
     refs.imageClose.addEventListener("click", closeGallModal);
@@ -35,10 +43,28 @@
       }
 
     function openGallModal() {
+        //open backdrop window
         refs.imageOpen.classList.toggle("is-oil-open");
+        document.body.style.position = 'fixed';
+      
+        //delete previously created element 
+        refs.backdropImagesContainer.removeChild(refs.backdropImagesContainer.lastChild);
+
+        //create new element 
+        let newElement = document.createElement("div");
+        //add class to new element class-attribute 
+        newElement.classList.add("backdrop-image-container");
+        newElement.innerHTML = this.innerHTML;
+        //add new element to backdrop window
+        refs.backdropImagesContainer.append(newElement); 
+        
+        const elementFirstChild = newElement.children[0];
+        elementFirstChild.classList.add("backdrop-img");  
+        // console.log(refs.backdropImagesContainer);
     }
 
     function closeGallModal() {
+        document.body.style.position = 'static';
         refs.imageOpen.classList.toggle("is-oil-open");
     }
 })();
